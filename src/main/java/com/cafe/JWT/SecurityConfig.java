83f8a983919ff01dbb2f37db8f614dd12318e7d7
akshaysadamategithub <akshaysadamatedeveloper@gmail.com>
2023-10-16 13:31:23 +0530

    first commit

---
+++ src/main/java/com/cafe/JWT/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.cafe.JWT;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig implements WebsecurityconfigurerAdapter {
+
+
+}
---
+++ src/main/java/com/cafe/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.cafe.dao;
+
+import com.cafe.POJO.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends JpaRepository<User,Integer> {
+
+    User findByEmailId(@Param("email") String email);
+}
